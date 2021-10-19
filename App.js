fdd383557df7d12bb8f02414678cd247694452db
Oğuzhan Bay <oguzhan.bay@diyalog.im>
2021-10-19 16:20:04 +0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  const globalScreenOptions = {
+    headerStyle: { backgroundColor: '#2c6bed' },
+    headerTitleStyle: { color: 'white' },
+    headerTintColor: 'white',
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnvyIMRQGRce1NYPEq5qbUHpQc78k5D6I",
+  authDomain: "signal-native-fc7c6.firebaseapp.com",
+  projectId: "signal-native-fc7c6",
+  storageBucket: "signal-native-fc7c6.appspot.com",
+  messagingSenderId: "1047848602810",
+  appId: "1:1047848602810:web:579c83d8695d9715ea4683"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
